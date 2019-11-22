ab8b3a8026827dbfa550c6bdfe89a58cfc818a65
iammahesh9919 <maheshpandey9919.github.com>
2019-11-22 10:35:14 +0530

    first commit

---
+++ src/Component/LoginSuccess.js
@@ -0,0 +1,54 @@
+import React, {Fragment,Component}from 'react';
+import MiniDrawer from '../Layout/Nav.js';
+import Order from '../Component/Order.js';
+import Inventory from '../Component/Inventory';
+import Home from '../Component/Home';
+import Shipment from '../Component/Shipment';
+import Mapshow from '../Component/Map';
+import Report from '../Component/Report';
+import FirstScreen from '../Component/FirstScreen';
+
+
+
+
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+//import Cube from './Demo.js';
+//<Demo />
+//<Mapshow />
+
+class LoginSuccess extends Component {
+ constructor(props) {
+    super(props);
+    
+  }
+
+  
+  render() 
+  {
+    
+    return (
+      <BrowserRouter>
+      <div>
+     
+     <MiniDrawer/>
+     
+       <Switch>
+          <Route path="/homepage" exact component={FirstScreen} />
+          <Route path="/StoreView" exact component={Home} />
+          <Route path="/Order"  component={Order} />
+          <Route path="/Inventory" component={Inventory} />  
+          <Route path="/Shipment" component={Shipment} />
+          <Route path="/Report" component={Report} />
+          
+
+        </Switch>
+
+      
+      </div>
+      </BrowserRouter>
+
+      );
+  }
+}
+export default LoginSuccess;
